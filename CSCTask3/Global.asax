edb982197cc0b840841079723b1e64249c84a9e6
Dennis <Dennis@DESKTOP-6T78R2I>
2020-06-26 23:42:36 +0800

    Add project files.

---
+++ CSCTask3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CSCTask3.WebApiApplication" Language="C#" %>
---
+++ CSCTask3/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(CSCTask3.Startup))]
+
+namespace CSCTask3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
